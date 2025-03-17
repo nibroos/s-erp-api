cdd48616037b51db675ae70331b0778caa1770d8
SyahiraIsnaeni <syahiraisnaeni15@gmail.com>
2025-03-17 15:43:33 +0700

    #27 | Syahira |  add store & update request po

---
+++ service/internal/validators/form_requests/purchase_order_store_request.go
@@ -0,0 +1,49 @@
+package form_requests
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/nibroos/s-erp-api/service/internal/dtos"
+	"github.com/nibroos/s-erp-api/service/internal/validators"
+)
+
+type PurchaseOrderStoreRequest struct {
+}
+
+func NewPurchaseOrderStoreRequest() *PurchaseOrderStoreRequest {
+	return &PurchaseOrderStoreRequest{}
+}
+
+func (r *PurchaseOrderStoreRequest) Validate(req *dtos.CreatePurchaseOrderRequest, ctx *fiber.Ctx) (map[string][]string, bool) {
+	rules := map[string][]string{
+		"customer_id":           []string{"required", "exists:customers,id"},
+		"purchase_type_id":      []string{"required", "exists:mix_values,id"},
+		"currency_id":           []string{"required", "exists:mix_values,id"},
+		"po_no":                 []string{"required"},
+		"po_date":               []string{"required", "date:yyyy-MM-dd"},
+		"delivery_date":         []string{"required", "date:yyyy-MM-dd"},
+		"status":                []string{"required"},
+		"po_dts":                []string{"array"},
+		"po_dts.*.product_id":   []string{"required", "exists:products,id"},
+		"po_dts.*.item_unit_id": []string{"exists:item_units,id"},
+		"po_dts.*.qty":          []string{"numeric"},
+		"po_dts.*.gen_code":     []string{},
+		"po_dts.*.remark":       []string{},
+	}
+
+	customFieldNames := map[string]string{}
+
+	var requestBody map[string]interface{}
+	if err := ctx.BodyParser(&requestBody); err != nil {
+		return map[string][]string{"error": {"Invalid request body"}}, false
+	}
+	request := validators.NewRequest(rules, requestBody, customFieldNames)
+	errors, valid := request.Validate()
+
+	convertedErrors := make(map[string][]string)
+	for key, value := range errors {
+		if len(value) > 0 {
+			convertedErrors[key] = value
+		}
+	}
+	return convertedErrors, valid
+}
