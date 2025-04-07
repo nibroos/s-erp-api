c785f0aff06d3ed5a6d588c784bfb8b9e4d676cf
SyahiraIsnaeni <syahiraisnaeni15@gmail.com>
2025-04-07 17:08:50 +0700

    #43 | Syahira | fix invoice dp repository

--- service/internal/repository/invoice_dp_repository.go
+++ service/internal/repository/invoice_dp_repository.go
@@ -46,7 +46,7 @@ func (r *InvoiceDpRepository) GetInvoiceDps(ctx *fiber.Ctx, filters map[string]s
 	var total int
 
 	filterDBColumnKey := []string{
-		"idp.invoice_no", "idp.remark",
+		"idp.invoice_no", "idp.remark", "idp.status",
 		"c.name",
 		"idt.remark",
 		"idtb.remark",
@@ -136,10 +136,10 @@ func (r *InvoiceDpRepository) GetInvoiceDps(ctx *fiber.Ctx, filters map[string]s
     FROM ( 
         SELECT DISTINCT ON (idp.id)
 					idp.id, idp.customer_id, idp.currency_id, idp.payment_term_id, idp.vat_id, idp.pph23_id, idp.branch_id,
-					idp.invoice_no, idp.remark, 
+					idp.invoice_no, idp.remark, idp.status, 
 					idp.exchange_rate, idp.pph23_percentage, idp.vat_percentage, idp.dp_percentage, idp.total_qty, idp.subtotal, idp.total_discount, idp.total_pph23, idp.total_vat, idp.grand_total, idp.created_by_id, idp.updated_by_id, idp.deleted_by_id, idp.created_at, idp.updated_at, idp.deleted_at,
 					TO_CHAR(idp.invoice_date, 'YYYY-MM-DD') as invoice_date,
-					idp.discount_amount, idp.discount_percentage, idp.discount_percentage_amount, idp.discount_final, idp.discount_type, idp.total_amount_products,
+					idp.discount_amount, idp.discount_percentage, idp.discount_percentage_amount, idp.discount_final, idp.discount_type, idp.total_amount_products, idp.total_dp_products,
 
 					c.name as customer_name,
 					cur.name as currency_name,
@@ -149,14 +149,12 @@ func (r *InvoiceDpRepository) GetInvoiceDps(ctx *fiber.Ctx, filters map[string]s
 					b.name as branch_name,
 
 					idt.remark as invoice_dp_dt_remark,
-					idtb.remark as invoice_dp_dt_bom_remark,
 
 					cu.name as created_by_name,
 					uu.name as updated_by_name
 
         FROM invoice_dps idp
 				LEFT JOIN invoice_dp_dts idt ON idt.invoice_dp_id = idp.id
-				LEFT JOIN invoice_dp_dt_boms idtb ON idtb.invoice_dp_dt_id = idt.id
 				LEFT JOIN customers c ON idp.customer_id = c.id
 				LEFT JOIN mix_values cur ON idp.currency_id = cur.id
 				LEFT JOIN mix_values pt ON idp.payment_term_id = pt.id
@@ -177,7 +175,7 @@ func (r *InvoiceDpRepository) GetInvoiceDps(ctx *fiber.Ctx, filters map[string]s
 
 	for key, value := range filters {
 		switch key {
-		case "invoice_no", "remark":
+		case "invoice_no", "remark", "status":
 			if value != "" {
 				query += fmt.Sprintf(" AND %s ILIKE $%d", key, i)
 				countQuery += fmt.Sprintf(" AND %s ILIKE $%d", key, i)
@@ -292,26 +290,31 @@ func (r *InvoiceDpRepository) GetInvoiceDpByID(ctx *fiber.Ctx, params *dtos.GetI
 
 	baseQuery := `
     FROM ( 
-			SELECT DISTINCT ON (idp.id)
-				idp.id, idp.customer_id, idp.currency_id, idp.payment_term_id, idp.vat_id, idp.pph23_id, idp.branch_id,
-				idp.invoice_no, idp.remark, 
-				idp.exchange_rate, idp.pph23_percentage, idp.vat_percentage, idp.dp_percentage, idp.total_qty, idp.subtotal, idp.total_discount, idp.total_pph23, idp.total_vat, idp.grand_total, idp.created_by_id, idp.updated_by_id, idp.deleted_by_id, idp.created_at, idp.updated_at, idp.deleted_at,
-				TO_CHAR(idp.invoice_date, 'YYYY-MM-DD') as invoice_date,
-				idp.discount_amount, idp.discount_percentage, idp.discount_percentage_amount, idp.discount_final, idp.discount_type, idp.total_amount_products,
+        SELECT DISTINCT ON (idp.id)
+            idp.id, idp.customer_id, idp.currency_id, idp.payment_term_id, idp.vat_id, idp.pph23_id, idp.branch_id,
+            idp.invoice_no, idp.remark, idp.status, 
+            idp.exchange_rate, idp.pph23_percentage, idp.vat_percentage, idp.dp_percentage, idp.total_qty, idp.subtotal, idp.total_discount, idp.total_pph23, idp.total_vat, idp.grand_total, idp.created_by_id, idp.updated_by_id, idp.deleted_by_id, idp.created_at, idp.updated_at, idp.deleted_at,
+            TO_CHAR(idp.invoice_date, 'YYYY-MM-DD') as invoice_date,
+            idp.discount_amount, idp.discount_percentage, idp.discount_percentage_amount, idp.discount_final, idp.discount_type, idp.total_amount_products, idp.total_dp_products,
 
-				cu.name as created_by_name,
-				uu.name as updated_by_name
+            cu.name as created_by_name,
+            uu.name as updated_by_name
 
-			FROM invoice_dps idp
-			LEFT JOIN invoice_dp_dts idt ON idt.invoice_dp_id = idp.id
-			LEFT JOIN invoice_dp_dt_boms idtb ON idtb.invoice_dp_dt_id = idt.id
+        FROM invoice_dps idp
+        LEFT JOIN invoice_dp_dts idt ON idt.invoice_dp_id = idp.id
+        LEFT JOIN users cu ON idp.created_by_id = cu.id
+        LEFT JOIN users uu ON idp.updated_by_id = uu.id
+    ) AS alias WHERE 1=1`
 
-			LEFT JOIN users cu ON idp.created_by_id = cu.id
-			LEFT JOIN users uu ON idp.updated_by_id = uu.id
-    ) AS alias WHERE 1=1 AND deleted_at IS NULL`
+	// Pindahkan kondisi deleted_at ke sini, seperti pada contoh PO
+	if params.IsDeleted != nil && *params.IsDeleted == 1 {
+		baseQuery += " AND deleted_at IS NOT NULL"
+	} else {
+		baseQuery += " AND deleted_at IS NULL"
+	}
 
 	query := `SELECT *
-		` + baseQuery
+        ` + baseQuery
 
 	var args []interface{}
 
@@ -326,13 +329,6 @@ func (r *InvoiceDpRepository) GetInvoiceDpByID(ctx *fiber.Ctx, params *dtos.GetI
 		i++
 	}
 
-	isDeletedQuery := ` AND deleted_at IS NULL`
-	if params.IsDeleted != nil && *params.IsDeleted == 1 {
-		isDeletedQuery = " AND deleted_at IS NOT NULL"
-	}
-
-	query += isDeletedQuery
-
 	if err := r.sqlDB.Get(&invoiceDp, query, args...); err != nil {
 		utils.LogErrors(childSpan, err)
 		childSpan.LogKV("query", query)
@@ -340,6 +336,54 @@ func (r *InvoiceDpRepository) GetInvoiceDpByID(ctx *fiber.Ctx, params *dtos.GetI
 	}
 
 	return &invoiceDp, nil
+}
+
+func (r *InvoiceDpRepository) GetUpdatedInvoiceDpDts(ctx *fiber.Ctx, invoiceDpID uint, span opentracing.Span) ([]dtos.InvoiceDpDtListUpdateDTO, error) {
+	childSpan := opentracing.StartSpan("InvoiceDpRepository-GetUpdatedInvoiceDpDts", opentracing.ChildOf(span.Context()))
+	defer childSpan.Finish()
+
+	invoiceDpDts := []dtos.InvoiceDpDtListUpdateDTO{}
+
+	query := `
+	SELECT 
+		idt.id, idt.product_uuid, idt.invoice_dp_id, idt.item_unit_id, idt.vat_id, idt.pph23_id, 
+		idt.ref_id, idt.ref_dt_id, idt.product_id, idt.ref_type, idt.product_type, idt.remark, 
+		idt.dp_percentage, idt.is_vat, idt.is_pph23, idt.qty, idt.price, idt.subtotal, 
+		idt.discount_amount, idt.discount_percentage, idt.discount_percentage_num, 
+		idt.discount_percentage_amount, idt.discount_final, idt.discount_type, 
+		idt.total_amount, idt.total_dp, idt.created_by_id, idt.updated_by_id, idt.deleted_by_id, 
+		idt.created_at, idt.updated_at, idt.deleted_at,
+		
+		idt.id as invoice_dp_dt_id,
+		isg.id as item_sub_group_id,
+		ig.id as item_group_id,
+		isg.name as item_sub_group_name,
+		ig.name as item_group_name,
+		u.name as unit_name,
+		p.name as item_name,
+		p.code as item_code,
+		
+		cu.name as created_by_name,
+		uu.name as updated_by_name
+	FROM invoice_dp_dts idt
+	LEFT JOIN products p ON idt.product_id = p.id
+	LEFT JOIN item_units iu ON idt.item_unit_id = iu.id
+	LEFT JOIN mix_values u ON iu.unit_id = u.id
+	LEFT JOIN mix_values isg ON p.item_sub_group_id = isg.id
+	LEFT JOIN mix_values ig ON isg.parent_id = ig.id
+	LEFT JOIN users cu ON idt.created_by_id = cu.id
+	LEFT JOIN users uu ON idt.updated_by_id = uu.id
+	WHERE idt.invoice_dp_id = $1 AND idt.deleted_at IS NULL
+	ORDER BY idt.id ASC
+	`
+
+	err := r.sqlDB.SelectContext(ctx.Context(), &invoiceDpDts, query, invoiceDpID)
+	if err != nil {
+		utils.LogErrors(childSpan, err)
+		return nil, err
+	}
+
+	return invoiceDpDts, nil
 }
 
 func (r *InvoiceDpRepository) BeginTransaction() *gorm.DB {
@@ -376,23 +420,6 @@ func (r *InvoiceDpRepository) CreateInvoiceDpDts(tx *gorm.DB, invoiceDpDts []mod
 	return tx, invoiceDpDts, nil
 }
 
-func (r *InvoiceDpRepository) BulkCreateInvoiceDpDtBoms(tx *gorm.DB, invoiceDpDtBoms []map[string]interface{}, span opentracing.Span) (*gorm.DB, error) {
-	childSpan := opentracing.StartSpan("InvoiceDpRepository-BulkCreateInvoiceDpDtBoms", opentracing.ChildOf(span.Context()))
-	defer childSpan.Finish()
-
-	if len(invoiceDpDtBoms) == 0 {
-		return tx, nil
-	}
-
-	result := tx.Table("invoice_dp_dt_boms").Create(invoiceDpDtBoms)
-	if result.Error != nil {
-		utils.LogErrors(childSpan, result.Error)
-		return tx, result.Error
-	}
-
-	return tx, nil
-}
-
 func (r *InvoiceDpRepository) GetInvoiceDpDts(ctx *fiber.Ctx, invoiceDpID uint, span opentracing.Span) ([]dtos.InvoiceDpDtListDTO, error) {
 	childSpan := opentracing.StartSpan("InvoiceDpRepository-GetInvoiceDpDts", opentracing.ChildOf(span.Context()))
 	defer childSpan.Finish()
@@ -402,15 +429,15 @@ func (r *InvoiceDpRepository) GetInvoiceDpDts(ctx *fiber.Ctx, invoiceDpID uint, 
 	query := `
 	SELECT 
 		idt.id, idt.product_uuid, idt.invoice_dp_id, idt.item_unit_id, idt.vat_id, idt.pph23_id, 
-		idt.ref_id, idt.product_id, idt.ref_type, idt.product_type, idt.remark, 
+		idt.ref_id, idt.ref_dt_id, idt.product_id, idt.ref_type, idt.product_type, idt.remark, 
 		idt.dp_percentage, idt.is_vat, idt.is_pph23, idt.qty, idt.price, idt.subtotal, 
 		idt.discount_amount, idt.discount_percentage, idt.discount_percentage_num, 
 		idt.discount_percentage_amount, idt.discount_final, idt.discount_type, 
 		idt.total_amount, idt.total_dp, idt.created_by_id, idt.updated_by_id, idt.deleted_by_id, 
 		idt.created_at, idt.updated_at, idt.deleted_at,
 		
-		p.name as product_name, p.code as product_code,
-		iu.name as unit_name,
+		p.name as item_name, p.code as item_code,
+		u.name as unit_name,
 		v.name as vat_name,
 		pph.name as pph23_name,
 		
@@ -419,6 +446,7 @@ func (r *InvoiceDpRepository) GetInvoiceDpDts(ctx *fiber.Ctx, invoiceDpID uint, 
 	FROM invoice_dp_dts idt
 	LEFT JOIN products p ON idt.product_id = p.id
 	LEFT JOIN item_units iu ON idt.item_unit_id = iu.id
+	LEFT JOIN mix_values u ON iu.unit_id = u.id
 	LEFT JOIN mix_values v ON idt.vat_id = v.id
 	LEFT JOIN mix_values pph ON idt.pph23_id = pph.id
 	LEFT JOIN users cu ON idt.created_by_id = cu.id
@@ -434,42 +462,6 @@ func (r *InvoiceDpRepository) GetInvoiceDpDts(ctx *fiber.Ctx, invoiceDpID uint, 
 	}
 
 	return invoiceDpDts, nil
-}
-
-func (r *InvoiceDpRepository) GetInvoiceDpDtBoms(ctx *fiber.Ctx, invoiceDpID uint, span opentracing.Span) ([]dtos.InvoiceDpDtBomListDTO, error) {
-	childSpan := opentracing.StartSpan("InvoiceDpRepository-GetInvoiceDpDtBoms", opentracing.ChildOf(span.Context()))
-	defer childSpan.Finish()
-
-	invoiceDpDtBoms := []dtos.InvoiceDpDtBomListDTO{}
-
-	query := `
-	SELECT 
-		idtb.id, idtb.product_uuid, idtb.invoice_dp_id, idtb.invoice_dp_dt_id, 
-		idtb.bom_id, idtb.product_id, idtb.item_unit_id, idtb.remark, 
-		idtb.qty, idtb.price, idtb.subtotal, idtb.created_by_id, idtb.updated_by_id, 
-		idtb.deleted_by_id, idtb.created_at, idtb.updated_at, idtb.deleted_at,
-		
-		p.name as product_name, p.code as product_code,
-		iu.name as unit_name,
-		
-		cu.name as created_by_name,
-		uu.name as updated_by_name
-	FROM invoice_dp_dt_boms idtb
-	LEFT JOIN products p ON idtb.product_id = p.id
-	LEFT JOIN item_units iu ON idtb.item_unit_id = iu.id
-	LEFT JOIN users cu ON idtb.created_by_id = cu.id
-	LEFT JOIN users uu ON idtb.updated_by_id = uu.id
-	WHERE idtb.invoice_dp_id = $1 AND idtb.deleted_at IS NULL
-	ORDER BY idtb.id ASC
-	`
-
-	err := r.sqlDB.SelectContext(ctx.Context(), &invoiceDpDtBoms, query, invoiceDpID)
-	if err != nil {
-		utils.LogErrors(childSpan, err)
-		return nil, err
-	}
-
-	return invoiceDpDtBoms, nil
 }
 
 func (r *InvoiceDpRepository) UpdateInvoiceDp(tx *gorm.DB, invoiceDp *models.InvoiceDp, span opentracing.Span) (*gorm.DB, error) {
@@ -521,45 +513,6 @@ func (r *InvoiceDpRepository) BulkUpdateInvoiceDpDts(tx *gorm.DB, invoiceDpDts [
 	return tx, nil
 }
 
-func (r *InvoiceDpRepository) BulkUpdateInvoiceDpDtBoms(tx *gorm.DB, invoiceDpDtBoms []map[string]interface{}, span opentracing.Span) (*gorm.DB, error) {
-	childSpan := opentracing.StartSpan("InvoiceDpRepository-BulkUpdateInvoiceDpDtBoms", opentracing.ChildOf(span.Context()))
-	defer childSpan.Finish()
-
-	if len(invoiceDpDtBoms) == 0 {
-		return tx, nil
-	}
-
-	for _, invoiceDpDtBom := range invoiceDpDtBoms {
-		result := tx.Model(&models.InvoiceDpDtBom{}).Where("id = ?", invoiceDpDtBom["id"]).Updates(invoiceDpDtBom)
-		if result.Error != nil {
-			utils.LogErrors(childSpan, result.Error)
-			return tx, result.Error
-		}
-	}
-
-	return tx, nil
-}
-
-func (r *InvoiceDpRepository) DeleteInvoiceDpDtBomsByIDs(tx *gorm.DB, invoiceDpDtBomIDs []uint, userID uint, span opentracing.Span) (*gorm.DB, error) {
-	childSpan := opentracing.StartSpan("InvoiceDpRepository-DeleteInvoiceDpDtBomsByIDs", opentracing.ChildOf(span.Context()))
-	defer childSpan.Finish()
-
-	if len(invoiceDpDtBomIDs) == 0 {
-		return tx, nil
-	}
-
-	result := tx.Model(&models.InvoiceDpDtBom{}).Where("id IN ?", invoiceDpDtBomIDs).Updates(map[string]interface{}{
-		"deleted_by_id": userID,
-		"deleted_at":    time.Now(),
-	})
-	if result.Error != nil {
-		utils.LogErrors(childSpan, result.Error)
-		return tx, result.Error
-	}
-
-	return tx, nil
-}
-
 func (r *InvoiceDpRepository) DeleteInvoiceDpDtsByIDs(tx *gorm.DB, invoiceDpDtIDs []uint, userID uint, span opentracing.Span) (*gorm.DB, error) {
 	childSpan := opentracing.StartSpan("InvoiceDpRepository-DeleteInvoiceDpDtsByIDs", opentracing.ChildOf(span.Context()))
 	defer childSpan.Finish()
@@ -594,6 +547,18 @@ func (r *InvoiceDpRepository) DeleteInvoiceDp(tx *gorm.DB, invoiceDpID uint, use
 	}
 
 	return tx, nil
+}
+
+func (r *InvoiceDpRepository) RestoreInvoiceDp(ctx *fiber.Ctx, params *dtos.GetInvoiceDpParams, tx *gorm.DB, span opentracing.Span) error {
+	childSpan := opentracing.StartSpan("InvoiceDpRepository-RestoreInvoiceDp", opentracing.ChildOf(span.Context()))
+	defer childSpan.Finish()
+
+	var invoiceDp models.InvoiceDp
+	if err := tx.Unscoped().Model(&invoiceDp).Where("id = ?", params.ID).Update("deleted_at", nil).Error; err != nil {
+		utils.LogErrors(childSpan, err)
+		return err
+	}
+	return nil
 }
 
 func (r *InvoiceDpRepository) GetRefSalesOrderDts(ctx *fiber.Ctx, filters map[string]string, span opentracing.Span) ([]dtos.RefSalesOrderDtListDTO, int, error) {
@@ -717,8 +682,8 @@ func (r *InvoiceDpRepository) GetRefSalesOrderDts(ctx *fiber.Ctx, filters map[st
 					TO_CHAR(so.due_at, 'YYYY-MM-DD') as due_at,
 
 					c.name as customer_name,
-					p.name as product_name, p.code as product_code, p.sku as item_sku,
-					iu.name as unit_name,
+					p.name as item_name, p.code as item_code,
+					u.name as unit_name,
 					v.name as vat_name,
 					pph.name as pph23_name,
 					
@@ -731,12 +696,13 @@ func (r *InvoiceDpRepository) GetRefSalesOrderDts(ctx *fiber.Ctx, filters map[st
 				LEFT JOIN customers c ON so.customer_id = c.id
 				LEFT JOIN products p ON sodt.item_id = p.id
 				LEFT JOIN item_units iu ON sodt.item_unit_id = iu.id
+				LEFT JOIN mix_values u ON iu.unit_id = u.id
 				LEFT JOIN mix_values v ON sodt.vat_id = v.id
 				LEFT JOIN mix_values pph ON sodt.pph23_id = pph.id
 
         LEFT JOIN users cu ON sodt.created_by_id = cu.id
         LEFT JOIN users uu ON sodt.updated_by_id = uu.id
-				WHERE 1=1 AND so.status = 'approved'` + condition + queryGlobal + `
+				WHERE 1=1 AND so.status IN ('PROCESS', 'DELIVERY', 'SCHEDULE', 'INVOICE')` + condition + queryGlobal + `
     ) AS alias WHERE 1=1 AND deleted_at IS NULL`
 
 	query := `SELECT *
@@ -848,34 +814,35 @@ func (r *InvoiceDpRepository) GetSoDtBoms(ctx *fiber.Ctx, soDtIDs []uint, span o
 	soDtBoms := []dtos.SalesOrderSoDtBomListDTO{}
 
 	query := `
-	SELECT 
-		sodtb.id, sodtb.product_uuid, sodtb.sales_order_id, sodtb.so_dt_id, 
-		sodtb.product_id, sodtb.item_id, sodtb.item_unit_id, sodtb.gen_code, sodtb.remark, 
-		sodtb.qty, sodtb.qty_out, sodtb.price_sell, sodtb.price_buy, 
-		sodtb.subtotal_sell, sodtb.subtotal_buy, sodtb.created_by_id, 
-		sodtb.updated_by_id, sodtb.deleted_by_id, sodtb.created_at, sodtb.updated_at, sodtb.deleted_at,
-		
-		p.name as product_name, p.code as product_code, p.sku as item_sku,
-		p.barcode as item_barcode, p.factory_code as item_factory_code,
-		p.specification as item_specification,
-		iu.name as unit_name,
-		
-		cu.name as created_by_name,
-		uu.name as updated_by_name
-	FROM so_dt_boms sodtb
-	LEFT JOIN products p ON sodtb.item_id = p.id
-	LEFT JOIN item_units iu ON sodtb.item_unit_id = iu.id
-	LEFT JOIN users cu ON sodtb.created_by_id = cu.id
-	LEFT JOIN users uu ON sodtb.updated_by_id = uu.id
-	WHERE sodtb.so_dt_id IN (?) AND sodtb.deleted_at IS NULL
-	ORDER BY sodtb.id ASC
-	`
+    SELECT 
+        sodtb.id, sodtb.product_uuid, sodtb.sales_order_id, sodtb.so_dt_id, 
+        sodtb.product_id, sodtb.item_id, sodtb.item_unit_id, sodtb.gen_code, sodtb.remark, 
+        sodtb.qty, sodtb.qty_out, sodtb.price_sell, sodtb.price_buy, 
+        sodtb.subtotal_sell, sodtb.subtotal_buy, sodtb.created_by_id, 
+        sodtb.updated_by_id, sodtb.deleted_by_id, sodtb.created_at, sodtb.updated_at, sodtb.deleted_at,
+        
+        p.name as item_name, p.code as item_code, p.sku as item_sku,
+        p.barcode as item_barcode, p.factory_code as item_factory_code,
+        p.specification as item_specification,
+        u.name as unit_name,
+        
+        cu.name as created_by_name,
+        uu.name as updated_by_name
+    FROM so_dt_boms sodtb
+    LEFT JOIN products p ON sodtb.item_id = p.id
+    LEFT JOIN item_units iu ON sodtb.item_unit_id = iu.id
+    LEFT JOIN mix_values u ON iu.unit_id = u.id
+    LEFT JOIN users cu ON sodtb.created_by_id = cu.id
+    LEFT JOIN users uu ON sodtb.updated_by_id = uu.id
+    WHERE sodtb.so_dt_id IN (?) AND sodtb.deleted_at IS NULL
+    ORDER BY sodtb.id ASC
+    `
 
 	query = r.db.ToSQL(func(tx *gorm.DB) *gorm.DB {
 		return tx.Raw(query, soDtIDs)
 	})
 
-	err := r.sqlDB.SelectContext(ctx.Context(), &soDtBoms, query, soDtIDs)
+	err := r.sqlDB.SelectContext(ctx.Context(), &soDtBoms, query)
 	if err != nil {
 		utils.LogErrors(childSpan, err)
 		return nil, err
