921fb8357d42ac66fe02e7310880a77a6cdad8bb
SyahiraIsnaeni <syahiraisnaeni15@gmail.com>
2025-04-12 10:08:34 +0700

    #43 | Syahira | add migration sales invoices

---
+++ service/internal/database/migrations/20250410072446_create_sales_invoices_table.down.sql
@@ -0,0 +1,5 @@
+BEGIN;
+
+DROP TABLE IF EXISTS sales_invoices;
+
+COMMIT;
---
+++ service/internal/database/migrations/20250410072446_create_sales_invoices_table.up.sql
@@ -0,0 +1,53 @@
+BEGIN;
+
+CREATE TABLE IF NOT EXISTS sales_invoices (
+  id SERIAL PRIMARY KEY,
+  customer_id INT REFERENCES customers(id) ON DELETE RESTRICT,
+  currency_id INT REFERENCES mix_values(id) ON DELETE RESTRICT,
+  payment_term_id INT REFERENCES mix_values(id) ON DELETE RESTRICT,
+  vat_id INT REFERENCES mix_values(id) ON DELETE RESTRICT,
+  pph23_id INT REFERENCES mix_values(id) ON DELETE RESTRICT,
+  branch_id INT REFERENCES branches(id) ON DELETE RESTRICT,
+  invoice_no TEXT,
+  invoice_date date,
+  exchange_rate DECIMAL(20, 5),
+  remark TEXT,
+  rev_no INT,
+  status TEXT,
+  pph23_percentage DECIMAL(20, 5),
+  vat_percentage DECIMAL(20, 5),
+  discount_amount DECIMAL(20, 5),
+  discount_percentage DECIMAL(20, 5),
+  discount_percentage_amount DECIMAL(20, 5),
+  discount_final DECIMAL(20, 5),
+  discount_type TEXT,
+  subtotal DECIMAL(20, 5),
+  total_amount_products DECIMAL(20, 5),
+  total_dp_products DECIMAL(20, 5),
+  total_balance_products DECIMAL(20, 5),
+  total_qty DECIMAL(20, 5),
+  total_discount DECIMAL(20, 5),
+  total_pph23 DECIMAL(20, 5),
+  total_vat DECIMAL(20, 5),
+  grand_total DECIMAL(20, 5),
+  created_by_id INT,
+  updated_by_id INT,
+  deleted_by_id INT,
+  created_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP,
+  updated_at timestamp with time zone,
+  deleted_at timestamp with time zone
+);
+
+CREATE INDEX idx_sales_invoices_customer_id ON sales_invoices(customer_id);
+
+CREATE INDEX idx_sales_invoices_currency_id ON sales_invoices(currency_id);
+
+CREATE INDEX idx_sales_invoices_payment_term_id ON sales_invoices(payment_term_id);
+
+CREATE INDEX idx_sales_invoices_vat_id ON sales_invoices(vat_id);
+
+CREATE INDEX idx_sales_invoices_pph23_id ON sales_invoices(pph23_id);
+
+CREATE INDEX idx_sales_invoices_branch_id ON sales_invoices(branch_id);
+
+COMMIT;
