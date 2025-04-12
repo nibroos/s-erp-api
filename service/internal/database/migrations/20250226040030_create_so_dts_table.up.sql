43198134f548c8dbee3927930458faf1ce01a063
SyahiraIsnaeni <syahiraisnaeni15@gmail.com>
2025-04-12 10:08:59 +0700

    #43 | Syahira | update migration so dt and comp profile

--- service/internal/database/migrations/20250226040030_create_so_dts_table.up.sql
+++ service/internal/database/migrations/20250226040030_create_so_dts_table.up.sql
@@ -45,6 +45,7 @@ CREATE TABLE IF NOT EXISTS so_dts (
   total_am DECIMAL(20, 5),
   si_total_am DECIMAL(20, 5),
   sa_total_am DECIMAL(20, 5),
+  total_dp DECIMAL(20, 5),
   created_by_id INT,
   updated_by_id INT,
   deleted_by_id INT,
